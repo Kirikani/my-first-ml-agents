27517c0973c75701d1a7da5c5079bb0c320ca6ee
Kirikani <kankirihachi@yahoo.co.jp>
2019-10-14 22:10:56 +0900

    ml-gaents first commit
    first ml

---
+++ Assets/RollerAcademy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class RollerAcademy : Academy
+{
+    // Start is called before the first frame update
+    
+    public override void InitializeAcademy()
+    {
+        base.InitializeAcademy();
+    }
+ 
+    public override void AcademyReset()
+    {
+        base.AcademyReset();
+    }
+ 
+    public override void AcademyStep()
+    {
+        base.AcademyStep();
+    }
+
+}
