589836fe0ea9c40725231aa5fd82df9783b229d6
Kirikani <kankirihachi@yahoo.co.jp>
2019-12-06 23:09:20 +0900

    1206

--- Assets/Script/TrowAwayHandAgent.cs
+++ Assets/Script/TrowAwayHandAgent.cs
@@ -10,6 +10,7 @@ public class TrowAwayHandAgent : Agent {
     int count;
     Rigidbody rBody;
     Transform tBody;
+    
     // Start is called before the first frame update
     void Start () {
         rBody = GetComponent<Rigidbody> ();
@@ -17,8 +18,16 @@ public class TrowAwayHandAgent : Agent {
         catchFlag = 0;
     }
 
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     if(collision.gameObject.tag == "Finish")
+    //     {
+    //         Debug.Log(Name+"OnCollision");
+    //     }
+    // }
+
+
     public override void AgentReset () {
-        Debug.Log ("Reset");
         tBall.position=new Vector3 (-1.0f, 5.0f, 0);
         rBall.position = new Vector3 (-1, 5, 0);
         rBody.position = new Vector3 (-1, 1, 0);
@@ -31,6 +40,7 @@ public class TrowAwayHandAgent : Agent {
 
     public override void CollectObservations () {
         AddVectorObs (rBall.position);
+        AddVectorObs (rBall.velocity);
         AddVectorObs (rBody.position);
         AddVectorObs (rBody.rotation);
         AddVectorObs (catchFlag);
@@ -39,7 +49,7 @@ public class TrowAwayHandAgent : Agent {
     public override void AgentAction (float[] vectorAction, string textAction) {
         Vector3 RSignal = Vector3.zero;
         Vector3 ForceSginal = Vector3.zero;
-        float distanceToBall = Vector3.Distance (tBall.position, rBody.position);
+        float distanceToBall = Vector3.Distance (rBall.position, rBody.position);
         count++;
 
         RSignal.x = vectorAction[0];
@@ -51,32 +61,34 @@ public class TrowAwayHandAgent : Agent {
         ForceSginal.z = vectorAction[5];
 
         rBody.angularVelocity = RSignal * 0.1f * Mathf.PI;
+        AddReward(rBall.velocity.x*2);
         if (catchFlag>4) {
             rBody.AddForce (ForceSginal*10, ForceMode.Impulse);
-            AddReward (RSignal.magnitude);
-            AddReward (tBall.position.x);
+            AddReward(rBall.velocity.x*5);
         } else {
             rBody.AddForce (ForceSginal*10,ForceMode.Force);            
-            AddReward (5-RSignal.magnitude);
-            if (distanceToBall < 0.1) {
+            if (distanceToBall < 0.1 && rBall.position.x>rBody.position.x+0.3) {
                 Debug.Log ("catch");
-                AddReward (100);
+                AddReward (1);
                 catchFlag++;
             }
         }
         if (tBall.position.y < 1.1) {
-            AddReward (tBall.position.x * 100);
-            Debug.Log ("Fall Ball");
+            if(tBall.position.x>0){
+                AddReward (tBall.position.x);
+            }
+            Debug.Log ("Fall Ball"+rBall.velocity.x);
             Done ();
         }
-        if (rBody.position.x > 0) {
-            AddReward (-100);
-            Debug.Log ("Hand out");
-            Done ();
+        if (rBody.position.x > 3) {
+            AddReward (-1);
+            Debug.Log ("Hand out"+rBall.velocity.x);
+            //Done ();
         }
-        if(count>100){
-            AddReward (-100);
-            Debug.Log ("Time out");
+        if(count>200){
+            AddReward (-1);
+            AddReward (tBall.position.x);
+            Debug.Log ("Time out"+rBall.velocity.x);
             Done();
         }
     }
